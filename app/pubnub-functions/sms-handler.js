4b706f6b6fbbeada2784aed5f9908ae8fb1afc50
Adam Bavosa <adamb@pubnub.com>
2018-02-13 22:55:58 -0800

    added helpful somments

--- app/pubnub-functions/sms-handler.js
+++ app/pubnub-functions/sms-handler.js
@@ -1,5 +1,7 @@
 const xhr = require('xhr');
 const basicAuth = require('codec/auth');
+
+// Use the PubNub Functions Vault Module to store these keys securely.
 const username = '__YOUR_CLICKSEND_USER_NAME__';
 const authKey = '__YOUR_CLICKSEND_AUTH_KEY__';
 const uri = 'https://rest.clicksend.com/v3/sms/send';
@@ -11,6 +13,7 @@ export default (request) => {
 
   // Ideally, the phone numbers would be in the PN request body or they could be
   // fetched from a secure endpoint right here using `return xhr.fetch().then()`
+  // Never hard code phone numbers in function code.
   let crowdPhoneNumbers = ['+19999999999'];
   let body = `Crowdsale "${crowdsaleName}" for TOK is now open!` +
     ` Ethereum Address: ${contractAddress}`;
